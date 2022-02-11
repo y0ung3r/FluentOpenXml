5bc8c2602ddb31434ad59c99b33ef093cf57766e
Никита Великий <mtyoungstz@outlook.com>
2022-02-12 00:37:46 +0500

    Добавлена единица измерения "Пика"

---
+++ FluentOpenXml/Units/Universal/Picas.cs
@@ -0,0 +1,28 @@
+﻿using FluentOpenXml.Units.OpenXml;
+
+namespace FluentOpenXml.Units.Universal;
+
+/// <summary>
+/// Представляет пики
+/// </summary>
+public sealed class Picas : UniversalUnits
+{
+	/// <summary>
+	/// Инициализирует <see cref="Picas"/>
+	/// </summary>
+	/// <param name="value">Значение</param>
+	public Picas(double value)
+		: base(value)
+	{ }
+
+	/// <summary>
+	/// Преобразовывает <see cref="Picas"/> в <see cref="Emu"/>
+	/// </summary>
+	/// <returns></returns>
+	internal Inches ToInches() => new Inches(Value / 6.0);
+
+	/// <summary>
+	/// Преобразовывает <see cref="Picas"/> в <see cref="Emu"/>
+	/// </summary>
+	internal override Emu ToEmu() => ToInches().ToEmu();
+}
